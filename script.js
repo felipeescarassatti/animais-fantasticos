03faf14c84ab58e32c4ae59f34cea1aef86ec147
Felipe Escarassatti <felipeescarassatti@gmail.com>
2021-08-31 17:31:43 -0300

    animacao do conteudo

--- script.js
+++ script.js
@@ -70,3 +70,29 @@ function initScrollSuave(){
   });
 }
 initScrollSuave();
+
+//Animação do conteudo do site conforme o scroll vai descendo
+function initAnimacaoScroll(){
+  const sections =  document.querySelectorAll('.js-scroll');
+  if(sections.length){
+    const windowMetade = window.innerHeight*0.6;
+
+    function animaScroll(){
+      sections.forEach((section)=>{
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop-windowMetade) < 0;
+        //console.log(sectionTop);
+        if(isSectionVisible){
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });  
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
