1aae55b6d12326ee6660bbf876041b353fe9f712
Felipe Escarassatti <felipeescarassatti@gmail.com>
2021-08-31 16:35:07 -0300

    animacao faq com js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function initTabNav() {
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
@@ -17,3 +18,27 @@ tabMenu.forEach((itemMenu, index)=> {
   });
 });
 }
+}
+initTabNav();
+
+function initAccordion(){
+const accordionList = document.querySelectorAll('.js-accordion dt');
+const activeClass = 'ativo';
+
+if(accordionList.length) {
+  accordionList[0].classList.add(activeClass);
+  accordionList[0].nextElementSibling.classList.add(activeClass);
+
+  function activeAccordion(){
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
+  }
+
+  accordionList.forEach((item)=>{
+    item.addEventListener('click', activeAccordion);
+  });
+  }
+}
+
+initAccordion();
+
