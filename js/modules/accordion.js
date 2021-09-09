0c88b5002fb82d5f7247432c55d686f565af3b75
Felipe Escarassatti <felipeescarassatti@gmail.com>
2021-09-09 15:07:31 -0300

    alterando para JS modules

---
+++ js/modules/accordion.js
@@ -0,0 +1,19 @@
+// Animação Faq de perguntar, clica na pergunta e aparece a resposta
+export default function initAccordion(){
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  const activeClass = 'ativo';
+  
+  if(accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+  
+    function activeAccordion(){
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+  
+    accordionList.forEach((item)=>{
+      item.addEventListener('click', activeAccordion);
+    });
+    }
+  }
