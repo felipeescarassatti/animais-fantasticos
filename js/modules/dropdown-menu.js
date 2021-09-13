8c76318044b8fb507bdaba66fbc3e61089a6cbf7
Felipe Escarassatti <felipeescarassatti@gmail.com>
2021-09-13 16:16:28 -0300

    dropdown menu pronto

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,22 @@
+import outsideClick from "./outsideclick.js";
+
 export default function initDropdownMenu() {
-  
+    const dropdownMenu =  document.querySelectorAll('[data-dropdown]');
+
+    dropdownMenu.forEach((menu)=> {
+      ['touchstart','click'].forEach((userEvent)=>{
+        menu.addEventListener(userEvent, handleClick);
+      })
+    })
+
+    function handleClick(event) {
+      event.preventDefault();
+      this.classList.toggle('active');
+      outsideClick(this, ['touchstart','click'], ()=> {
+        this.classList.remove('active');
+      });
+    }
 }
+
+
+
